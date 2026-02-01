f423a6e90a32d37ad4b23642a1a67667515127cc
thawk <thawk009@gmail.com>
2026-02-01 23:05:36 +0800

    update

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,3 +1,6 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+if vim.env.PYTHON3_HOST_PROG and vim.env.PYTHON3_HOST_PROG ~= "" then
+  vim.g.python3_host_prog = vim.env.PYTHON3_HOST_PROG
+end
