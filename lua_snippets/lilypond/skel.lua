aa577a8d2518333570ee2741f7efe14271c904a2
thawk <thawk009@gmail.com>
2026-09-20 23:04:53 +0800

    update

--- lua_snippets/lilypond/skel.lua
+++ lua_snippets/lilypond/skel.lua
@@ -10,7 +10,10 @@ local fmt = require("luasnip.extras.fmt").fmt
 return {
 
   -- skel Notes
-  s({ trig = [[skel]], desc = [[Notes]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Notes]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -48,20 +51,33 @@ return {
 "@1?MelodyC" = \"@1?NotesC"
 "@1?Melody" = \"@1?Notes"
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [4] = i(4, "c''"),
-  [5] = i(5, "4/4"),
-  x1 = f(function(args) return (args[1][1] or ""):gsub("[,']+", "") end, {6}),
-  [7] = i(7, "\\major"),
-  [0] = i(0),
-  [8] = i(8, "60"),
-  [6] = i(6, "c"),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [4] = i(4, "c''"),
+        [5] = i(5, "4/4"),
+        x1 = f(function(args)
+          return (args[1][1] or ""):gsub("[,']+", "")
+        end, { 6 }),
+        [7] = i(7, "\\major"),
+        [0] = i(0),
+        [8] = i(8, "60"),
+        [6] = i(6, "c"),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel Notes with lyric
-  s({ trig = [[skel]], desc = [[Notes with lyric]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Notes with lyric]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -130,21 +146,36 @@ melody = \relative @4?
 	}
 }
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [4] = i(4, "c''"),
-  [5] = i(5, "4/4"),
-  x1 = f(function(args) return (args[1][1] or ""):gsub("[,']+", "") end, {6}),
-  [7] = i(7, "\\major"),
-  [0] = i(0),
-  [8] = i(8, "60"),
-  [6] = i(6, "c"),
-  x2 = f(function(args) return args[1][1] or "" end, {9}),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [4] = i(4, "c''"),
+        [5] = i(5, "4/4"),
+        x1 = f(function(args)
+          return (args[1][1] or ""):gsub("[,']+", "")
+        end, { 6 }),
+        [7] = i(7, "\\major"),
+        [0] = i(0),
+        [8] = i(8, "60"),
+        [6] = i(6, "c"),
+        x2 = f(function(args)
+          return args[1][1] or ""
+        end, { 9 }),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel Parallel Notes (one staff)
-  s({ trig = [[skel]], desc = [[Parallel Notes (one staff)]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Parallel Notes (one staff)]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -183,18 +214,29 @@ melody = \relative @4?
 
 "@1?Melody" = \"@1?Notes"
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [0] = i(0),
-  [4] = i(4, "108"),
-  [5] = i(5, "c"),
-  [6] = i(6, "c"),
-  [7] = i(7, "\\major"),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [0] = i(0),
+        [4] = i(4, "108"),
+        [5] = i(5, "c"),
+        [6] = i(6, "c"),
+        [7] = i(7, "\\major"),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel Parallel Notes (1 staffs with lyric)
-  s({ trig = [[skel]], desc = [[Parallel Notes (1 staffs with lyric)]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Parallel Notes (1 staffs with lyric)]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -308,19 +350,32 @@ grayVoice = {
 	>>
 }
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [0] = i(0),
-  [4] = i(4, "60"),
-  [5] = i(5, "c"),
-  [6] = i(6, "c"),
-  [7] = i(7, "\\major"),
-  x1 = f(function(args) return args[1][1] or "" end, {8}),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [0] = i(0),
+        [4] = i(4, "60"),
+        [5] = i(5, "c"),
+        [6] = i(6, "c"),
+        [7] = i(7, "\\major"),
+        x1 = f(function(args)
+          return args[1][1] or ""
+        end, { 8 }),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel Parallel Notes (two staffs)
-  s({ trig = [[skel]], desc = [[Parallel Notes (two staffs)]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Parallel Notes (two staffs)]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -365,18 +420,29 @@ grayVoice = {
 
 "@1?Melody" = \"@1?Notes"
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [0] = i(0),
-  [4] = i(4, "108"),
-  [5] = i(5, "c"),
-  [6] = i(6, "c"),
-  [7] = i(7, "\\major"),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [0] = i(0),
+        [4] = i(4, "108"),
+        [5] = i(5, "c"),
+        [6] = i(6, "c"),
+        [7] = i(7, "\\major"),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel Parallel Notes (two staffs separated)
-  s({ trig = [[skel]], desc = [[Parallel Notes (two staffs separated)]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[Parallel Notes (two staffs separated)]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -418,18 +484,29 @@ grayVoice = {
 
 "@1?Melody" = \"@1?Notes"
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [0] = i(0),
-  [4] = i(4, "108"),
-  [5] = i(5, "c"),
-  [6] = i(6, "c"),
-  [7] = i(7, "\\major"),
-}, { delimiters = "@?", repeat_duplicates = true })),
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [0] = i(0),
+        [4] = i(4, "108"),
+        [5] = i(5, "c"),
+        [6] = i(6, "c"),
+        [7] = i(7, "\\major"),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
   -- skel 普通高低音部，带和弦和歌词
-  s({ trig = [[skel]], desc = [[普通高低音部，带和弦和歌词]] }, fmt([[
+  s(
+    { trig = [[skel]], desc = [[普通高低音部，带和弦和歌词]] },
+    fmt(
+      [[
 "@1?Piece" = "@2?"
 "@1?Opus" = ""
 "@1?Composer" = ""
@@ -489,19 +566,30 @@ grayVoice = {
 
 "@1?Melody" = \"@1?Notes"
 
-]], {
-  [1] = d(1, function(args, parent) return sn(nil, { i(1, vim.fn.expand("%:t:r")) }) end),
-  [2] = d(2, function(args, parent) return sn(nil, { i(2, args[1][1] or "") }) end, {1}),
-  [3] = i(3, "C"),
-  [4] = i(4, "c''"),
-  [5] = i(5, "4/4"),
-  x1 = f(function(args) return (args[1][1] or ""):gsub("[,']+", "") end, {6}),
-  [7] = i(7, "\\major"),
-  [0] = i(0),
-  [8] = i(8, "c"),
-  x2 = f(function(args) return (args[1][1] or ""):gsub("[,']+", "") end, {9}),
-  [6] = i(6),
-  [9] = i(9, "108"),
-}, { delimiters = "@?", repeat_duplicates = true })),
-
+]],
+      {
+        [1] = d(1, function(args, parent)
+          return sn(nil, { i(1, vim.fn.expand("%:t:r")) })
+        end),
+        [2] = d(2, function(args, parent)
+          return sn(nil, { i(2, args[1][1] or "") })
+        end, { 1 }),
+        [3] = i(3, "C"),
+        [4] = i(4, "c''"),
+        [5] = i(5, "4/4"),
+        x1 = f(function(args)
+          return (args[1][1] or ""):gsub("[,']+", "")
+        end, { 6 }),
+        [7] = i(7, "\\major"),
+        [0] = i(0),
+        [8] = i(8, "c"),
+        x2 = f(function(args)
+          return (args[1][1] or ""):gsub("[,']+", "")
+        end, { 9 }),
+        [6] = i(6),
+        [9] = i(9, "108"),
+      },
+      { delimiters = "@?", repeat_duplicates = true }
+    )
+  ),
 }
